ef448bdc15dce1523b06b92103f88ef0ea5aa4f9
chengjian <2280896448@qq.com>
2023-06-03 11:46:07 +0800

    完成P17

--- src/main/java/com/roadjava/handler/MainViewHandler.java
+++ src/main/java/com/roadjava/handler/MainViewHandler.java
@@ -3,8 +3,10 @@ package com.roadjava.handler;
 import com.roadjava.entity.AdminDO;
 import com.roadjava.service.AdminService;
 import com.roadjava.service.impl.AdminServiceImpl;
+import com.roadjava.student.view.AddSRView;
 import com.roadjava.student.view.LoginView;
 import com.roadjava.student.view.MainView;
+import com.roadjava.student.view.UpdateSRView;
 
 import javax.swing.*;
 import javax.swing.text.View;
@@ -23,9 +25,9 @@ public class MainViewHandler extends KeyAdapter implements ActionListener {
         JButton jButton = (JButton) e.getSource();
         String text = jButton.getText();
         if("增加".equals(text)){
-
+            new AddSRView(mainView);
         }else if("修改".equals(text)){
-
+            new UpdateSRView(mainView);
         }else if("删除".equals(text)){
 
         }else if("查询".equals(text)){
